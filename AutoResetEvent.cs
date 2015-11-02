fedb9a373fd40f273c49fa247f1b366582926137
Atif Aziz <code@raboof.com>
2015-11-02 09:41:31 +0100

    Unused import

--- AutoResetEvent.cs
+++ AutoResetEvent.cs
@@ -20,7 +20,6 @@ namespace Worms
 
     using System;
     using System.Diagnostics;
-    using System.Runtime.CompilerServices;
     using System.Threading;
     using System.Threading.Tasks;
     using AngryArrays.Push;
