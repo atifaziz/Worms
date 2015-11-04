219530facd111c000a5e8ccbaca2bcd0551b0abe
Atif Aziz <code@raboof.com>
2015-11-04 18:51:00 +0100

    Use commit time (sortable) instead of hash for edge package version string

--- pack.cmd
+++ pack.cmd
@@ -13,17 +13,18 @@ set COMMIT_UNIX_TIME=
 if not defined COMMIT_UNIX_TIME echo Unable to determine the time of commit. Git not installed? & exit /b 1
 call :dtjs %COMMIT_UNIX_TIME% > "%temp%\dt.js"
 for /f %%i in ('cscript //nologo "%temp%\dt.js"') do set COMMIT_TIME=%%i
-set COMMIT_HASH=
-for /f %%i in ('git log -1 "--format=%%H"') do set COMMIT_HASH=%%i
-if not defined COMMIT_HASH echo Unable to determine the commit hash & exit. Git not installed? /b 1
-set COMMIT_HASH=%COMMIT_HASH:~0,4%
-set VERSION_SUFFIX=%VERSION_SUFFIX%-alpha-%COMMIT_TIME%-%COMMIT_HASH%
+set VERSION_SUFFIX=%VERSION_SUFFIX%-alpha-%COMMIT_TIME:~0,8%T%COMMIT_TIME:~-4%
 :pack
 call build && NuGet pack Worms.nuspec -Symbol -Properties "%VERSION_SUFFIX%"
 goto :EOF
 
 :dtjs
-echo var d = new Date(0);
-echo d.setUTCSeconds(%1);
-echo WScript.Echo(d.getUTCFullYear() + ('0' + (d.getUTCMonth() + 1)).slice(-2) + ('0' + d.getUTCDate()).slice(-2))
+echo Number.prototype.ra = function(w) { return (w + this.valueOf()).slice(-w.length); }
+echo var dt = new Date(0); dt.setUTCSeconds(%1);
+echo var y  = dt.getUTCFullYear() ,
+echo     mo = dt.getUTCMonth() + 1,
+echo     d  = dt.getUTCDate()     ,
+echo     h  = dt.getUTCHours()    ,
+echo     mi = dt.getUTCMinutes()  ;
+echo WScript.Echo(y + mo.ra('00') + d.ra('00') + h.ra('00') + mi.ra('00'));
 goto :EOF
