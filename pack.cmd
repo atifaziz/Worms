94895d2074921f1f442d8aed79d9a67915fdc161
Atif Aziz <code@raboof.com>
2015-11-27 08:45:50 +0100

    Commit hash as NuGet package release notes

--- pack.cmd
+++ pack.cmd
@@ -8,14 +8,24 @@ goto :EOF
 :main
 setlocal
 set VERSION_SUFFIX=VersionSuffix=
+set REPO_COMMIT=RepoCommit=N/A
+
 set COMMIT_UNIX_TIME=
 (for /f %%i in ('git log -1 "--format=%%ct"') do set COMMIT_UNIX_TIME=%%i) || goto :pack
 if not defined COMMIT_UNIX_TIME echo Unable to determine the time of commit. Git not installed? & exit /b 1
 call :dtjs %COMMIT_UNIX_TIME% > "%temp%\dt.js"
 for /f %%i in ('cscript //nologo "%temp%\dt.js"') do set COMMIT_TIME=%%i
-set VERSION_SUFFIX=%VERSION_SUFFIX%-alpha-%COMMIT_TIME:~0,8%T%COMMIT_TIME:~-4%
+set VERSION_SUFFIX=VersionSuffix=-alpha-%COMMIT_TIME:~0,8%T%COMMIT_TIME:~-4%
+
+set COMMIT_HASH=
+(for /f %%i in ('git log -1 "--format=%%H"') do set COMMIT_HASH=%%i) || goto :pack
+if not defined COMMIT_HASH echo Unable to determine the Git commit hash. & exit /b 1
+set REPO_COMMIT=RepoCommit=%COMMIT_HASH%
+
 :pack
-call build && NuGet pack Worms.nuspec -Symbol -Properties "%VERSION_SUFFIX%"
+call build ^
+  && (if not exist dist mkdir dist) ^
+  && NuGet pack Worms.nuspec -Symbol -Properties "%VERSION_SUFFIX%;%REPO_COMMIT%" -OutputDirectory dist
 goto :EOF
 
 :dtjs
